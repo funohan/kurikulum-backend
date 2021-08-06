61498ceb336ca804145535c27280d4d2749eca93
funohan <kazuyagaming66@gmail.com>
2021-08-06 19:22:08 +0900

    pengumpulan tugas

--- Sprint-3/Topik-5/Latihan-5.2.php
+++ Sprint-3/Topik-5/Latihan-5.2.php
@@ -3,43 +3,43 @@
 //=================================MSQLI===============================================
 
 
-// $servername = "localhost";
-// $username = "funouhan";
-// $password = "funouhan";
-// $database = "PondokIT";
+$servername = "localhost";
+$username = "funouhan";
+$password = "funouhan";
+$database = "PondokIT";
 
-// // membuat koneksi
-// $conn = mysqli_connect ($servername, $username, $password, $databases);
+// membuat koneksi
+$conn = mysqli_connect ($servername, $username, $password, $database);
 
-// // mengecek koneksi
-// if (!$conn) {
-//     die("[---------------------------Connection failed Gaes.!!!---------------------------]" . mysqli_connect_error()); 
-// }
-// echo "[---------------------------Connection succesfully---------------------------]";
-// mysqli_close($conn);
+// mengecek koneksi
+if (!$conn) {
+    die("[---------------------------Connection failed Gaes.!!!---------------------------]" . mysqli_connect_error()); 
+}
+echo "[---------------------------Connection succesfully---------------------------]";
+mysqli_close($conn);
 
 //=====================================================================================
 
 //================================PDO==================================================
 
 
-$servername = "localhost";
-$username = "funouhan";
-$password = "funouhan";
-// $database = "PondokIT";
+// $servername = "localhost";
+// $username = "funouhan";
+// $password = "funouhan";
+// // $database = "PondokIT";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbnama=PondokIT", $username, $password);
-    // errornya
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // jika berhasil
-    echo "[-----------------------------Connection successfully---------------------------]"; 
-    }
-catch(PDOException $e)
-    {
-    // jika gagal 
-    echo "[---------------------------Connection failed Gaes.!!!---------------------------] " . $e->getMessage();
-    }
+// try {
+//     $conn = new PDO("mysql:host=$servername;dbnama=PondokIT", $username, $password);
+//     // errornya
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     // jika berhasil
+//     echo "[-----------------------------Connection successfully---------------------------]"; 
+//     }
+// catch(PDOException $e)
+//     {
+//     // jika gagal 
+//     echo "[---------------------------Connection failed Gaes.!!!---------------------------] " . $e->getMessage();
+//     }
  
 //======================================================================================
 

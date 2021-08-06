8c28e8f8c33a087c95531b7e81cc8bd568317e1a
funohan <kazuyagaming66@gmail.com>
2021-08-06 16:55:09 +0900

    pengumpulan tugas

---
+++ Sprint-3/Topik-5/Latihan-5.2.php
@@ -0,0 +1,46 @@
+<?php
+
+//=================================MSQLI===============================================
+
+
+// $servername = "localhost";
+// $username = "funouhan";
+// $password = "funouhan";
+// $database = "PondokIT";
+
+// // membuat koneksi
+// $conn = mysqli_connect ($servername, $username, $password, $databases);
+
+// // mengecek koneksi
+// if (!$conn) {
+//     die("[---------------------------Connection failed Gaes.!!!---------------------------]" . mysqli_connect_error()); 
+// }
+// echo "[---------------------------Connection succesfully---------------------------]";
+// mysqli_close($conn);
+
+//=====================================================================================
+
+//================================PDO==================================================
+
+
+$servername = "localhost";
+$username = "funouhan";
+$password = "funouhan";
+// $database = "PondokIT";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbnama=PondokIT", $username, $password);
+    // errornya
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // jika berhasil
+    echo "[-----------------------------Connection successfully---------------------------]"; 
+    }
+catch(PDOException $e)
+    {
+    // jika gagal 
+    echo "[---------------------------Connection failed Gaes.!!!---------------------------] " . $e->getMessage();
+    }
+ 
+//======================================================================================
+
+?>

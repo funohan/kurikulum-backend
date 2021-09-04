3b020206cf83fbe0663e8d73df414778a0821b5c
funohan <kazuyagaming66@gmail.com>
2021-09-04 12:41:44 +0900

    pengumpulan tugas

---
+++ Evaluasi/Evaluasi-4/Evaluasi-4.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "funouhan";
+$password = "funouhan";
+$dbname = "SantriIT";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mahasantri 2021</title>
+</head>
+<body>
+    <h2>Data SantriIT</h2>
+    <table>
+        <td>Nama</td>
+        <td>Umur</td>
+        <td>Asal</td>
+        <td>Divisi</td>
+    </table>
+</body>
+</html>

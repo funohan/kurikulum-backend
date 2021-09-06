4ff4aa0932144f5827396bc3c5a083de53ae16c6
funohan <kazuyagaming66@gmail.com>
2021-09-06 15:51:25 +0900

    evaluasiBulanan1

---
+++ EvaluasiBulanan/EvaluasiBulan1.php
@@ -0,0 +1,25 @@
+<?php
+
+for ($y=1; $y=10; $y++ ){
+    if ($x=1; $x=10; $x++)
+}
+
+class penjumlahan{
+    public function__construct{
+
+    }
+    public static function [
+
+    ]
+}
+
+$servername = "localhost";
+$username = "zstyle";
+$password = "123456789";
+$dbname = "Train";
+
+try {
+    $conn = new PDO("mysql:host;dbname=Train, $username, $password")
+}
+
+?>
